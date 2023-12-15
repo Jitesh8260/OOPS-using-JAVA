c36c380ca0b95a4938451beb03da9cbbcbbf9450
Jitesh Bhakat <137819786+Jitesh8260@users.noreply.github.com>
2023-12-15 22:11:39 +0530

    Add files via upload

---
+++ seriessum.java
@@ -0,0 +1,18 @@
+//package JAVA ASSIGNMENT 1;
+import java.util.Scanner;
+public class seriessum {
+    public static void main(String[] args) {
+        double i,x,n;
+        double sum=0,fact=1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number upto which u want series sum");
+        n=sc.nextInt();
+        System.out.println("Enter the value of x");
+        x=sc.nextInt();
+        for(i=1;i<=n;i++){
+            fact=fact*i;
+            sum=sum+((Math.pow(x,i))/(fact));
+        }
+        System.out.println("Sum of the series is:"+sum);
+    }
+}
---
+++ squarenumber.java
@@ -0,0 +1,15 @@
+
+//Program to print out the square numbers, ten in a line, without using multiplication operator !
+
+public class squarenumber {
+    public static void main(String[] args) {
+        int i,j,k=1,sum=0;
+        for(i=1;i<=10;i++){
+        System.out.println(sum);
+         sum=sum+k;
+         k=k+2;
+
+        }
+    }
+    
+}
