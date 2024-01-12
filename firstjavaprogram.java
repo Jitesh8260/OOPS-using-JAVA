a86206c463ccd4176ae183cc6fe7b0d5876ec9e4
Jitesh Bhakat <137819786+Jitesh8260@users.noreply.github.com>
2024-01-12 22:56:06 +0530

    Update firstjavaprogram.java

--- firstjavaprogram.java
+++ firstjavaprogram.java
@@ -2,7 +2,7 @@
 class firstjavaprogram{
     public static void main(String []args) {
         int i;
-        System.out.println("This is my first Java Program");
+        System.out.println("This is my  first Java Program");
         System.out.println("Printing 10 times"); 
         for(i=0;i<10;i++)
         {
